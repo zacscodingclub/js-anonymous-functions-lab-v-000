bccab55aee55f28ae0041fbbac24bce8e79583d5
zacscodingclub <zbaston@gmail.com>
2016-07-06 15:26:09 -0500

    Done.

--- js/jukebox.js
+++ js/jukebox.js
@@ -14,31 +14,39 @@ function randomNumber() {
   return Math.floor(Math.random());
 }
 
+function handleMenuClick() {
+  $('#menuOptions').html("You can play a song, pause a song, or view all the songs");
+}
+
 function menu() {
-  $('#menu').on('click', function(e) {
-    $('#menuOptions').html("You can play a song, pause a song, or view all the songs");
-  });
+  $('#menu').on('click', handleMenuClick);
+}
+
+function handlePlayClick() {
+  $('#songPlaying').html("now playing " + songToPlay + " by " + theArtist);
+  return "now playing " + songToPlay + " by " + theArtist;
 }
 
 function play() {
-  $('#play').on('click', function(e) {
-    $('#songPlaying').html("now playing " + songToPlay + " by " + theArtist);
-    return "now playing " + songToPlay + " by " + theArtist;
-  });
+  $('#play').on('click', handlePlayClick);
+}
+
+function handlePauseClick() {
+  $('#songPaused').html(songToPlay + " is paused");
 }
 
 function pause() {
-  $('#pause').on('click', function(e) {
-    $('#songPaused').html(songToPlay + " is paused");
-  });
+  $('#pause').on('click', handlePauseClick);
+}
+
+function handleShowClick() {
+  var songs = "";
+  for (var key in jukeboxSongs) {
+    songs += jukeboxSongs[key] + " by " + key + " is available to play. "
+  }
+  $('#showSongs').html(songs);
 }
 
 function show() {
-  $('#show').on('click', function(e) {
-    var songs = "";
-    for (var key in jukeboxSongs) {
-      songs += jukeboxSongs[key] + " by " + key +  " is available to play. "
-    }
-    $('#showSongs').html(songs);
-  });
+  $('#show').on('click', handleShowClick);
 }
